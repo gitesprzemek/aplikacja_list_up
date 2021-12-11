ec4abcaf4bdfa49eab5e21534abcf5de69a9c2d0
przemek <przemasik8321@gmail.com>
2021-12-11 17:17:14 +0100

    test commit

--- organizer/app/src/main/java/test/organizer/Registration.java
+++ organizer/app/src/main/java/test/organizer/Registration.java
@@ -3,15 +3,18 @@ package test.organizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
+    }
+    public void onBtnZarClick(View view)
+    {
+        finish();
 
     }
 }
