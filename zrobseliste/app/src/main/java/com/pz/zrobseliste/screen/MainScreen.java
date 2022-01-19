eb13f3c74e82353aa147a7a452737a4491324f6f
Mateusz Sachajko <mateuszsachajko@gmail.com>
2022-01-19 22:01:49 +0100

    swipe

--- zrobseliste/app/src/main/java/com/pz/zrobseliste/screen/MainScreen.java
+++ zrobseliste/app/src/main/java/com/pz/zrobseliste/screen/MainScreen.java
@@ -1,20 +1,20 @@
 package com.pz.zrobseliste.screen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.widget.RelativeLayout;
 
 import com.pz.zrobseliste.R;
 import com.pz.zrobseliste.utils.SwipeListener;
-import com.pz.zrobseliste.utils.SwipeType;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    SwipeListener swipeListener;
-    RelativeLayout relativeLayout;
+    private SwipeListener swipeListener;
+    private GestureDetectorCompat detector;
 
 
     @Override
@@ -22,21 +22,61 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        relativeLayout = findViewById(R.id.relative_layout);
-        swipeListener = new SwipeListener(relativeLayout);
+        swipeListener = new SwipeListener();
+
+        detector = new GestureDetectorCompat(this, this);
     }
 
+    @Override
     public boolean onTouchEvent(MotionEvent touchEvent) {
 
-        if(swipeListener.getSwipeType() == SwipeType.RIGHT){
-            startActivity(new Intent(MainScreen.this, GroupsScreen.class));
-            return true;
-        } else if(swipeListener.getSwipeType() == SwipeType.LEFT){
-            startActivity(new Intent(MainScreen.this, AllTasksScreen.class));
-            return true;
+        detector.onTouchEvent(touchEvent);
+        return super.onTouchEvent(touchEvent);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float x1 = e1.getX();
+        float y1 = e1.getY();
+        float x2 = e2.getX();
+        float y2 = e2.getY();
+
+        swipeListener.setSwipeType(x1, y1, x2, y2, velocityX, velocityY);
+
+        switch (swipeListener.getSwipeType()){
+            case LEFT:
+                startActivity(new Intent(MainScreen.this, AllTasksScreen.class));
+                return true;
+            case RIGHT:
+                startActivity(new Intent(MainScreen.this, GroupsScreen.class));
+                return true;
         }
 
-
-        return true;
+        return false;
     }
 }
